facefacb2c38fd6e7732ffb2a0463e742e37ff93
rafacpito <couto1105@gmail.com>
2019-10-22 17:58:26 -0300

    Trocando db para postgres

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :services
-  resources :customers
+  namespace :api do
+    namespace :v1 do
+      resources :services
+      resources :customers
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
