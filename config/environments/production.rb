66d4fe10f347c1753db2e4e08265422afbe60c31
rafacpito <38329758+rafacpito@users.noreply.github.com>
2020-10-21 20:32:24 -0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   
-  config.action_dispatch.tld_length = 2
+  config.action_dispatch.tld_length = 3
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
